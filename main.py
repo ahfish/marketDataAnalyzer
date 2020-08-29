b5e42f4d2c067eafde94c2cef854eebe6ac9887b
ahfish <ahfish@gmail.com>
2020-08-29 20:37:15 +0800

    file

--- main.py
+++ main.py
@@ -11,15 +11,11 @@ import pandas as pd
 
 logFormat = '%(asctime)s - %(levelname)s - %(threadName)s - [%(message)s]'
 logFormatter = logging.Formatter(logFormat)
-logging.basicConfig(filename='test.log', format=logFormat, level=logging.INFO)
+logging.basicConfig(filename='test.log', format=logFormat, level=logging.DEBUG)
 consoleHandler = logging.StreamHandler(sys.stdout)
 consoleHandler.setFormatter(logFormatter)
 rootLogger = logging.getLogger()
 rootLogger.addHandler(consoleHandler)
-
-
-def f(x):
-    return x*x
 
 
 class MATCH_RESULT(Enum):
@@ -53,16 +49,47 @@ def enrich(data):
     data['monthGroup'] = date_time.strftime("%Y-%m")
 
 
-def process_data(data):
-    up_pt = 40
-    down_pt = 10
+def down_up_strategy(down_pt, up_pt, data):
+    if data:
+        start_pt = data[0]
+        first_date = start_pt['timeObj']
+        data_after = [x for x in data if x['timeObj'] > first_date]
+        first_match = next((x for x in data_after if start_pt['high'] - down_pt * 0.0001 >= x['low']), None)
+        debug(
+            f"Data - {start_pt['timeObj']}, detect when get lower than {start_pt['high'] - down_pt * 0.0001}")
+        if first_match is not None:
+            debug(
+                f"First Match - {first_match['timeObj']}, try seek higher than {first_match['low'] + up_pt * 0.0001}")
+            list_after = [x for x in data_after if x['timeObj'] > first_match['timeObj']]
+            second_match = next((x for x in list_after if first_match['low'] + up_pt * 0.0001 <= x['high']), None)
+            if second_match is not None:
+                debug(f"Second Match - {second_match}")
+                return MATCH_RESULT.MATCH
+            else:
+                return MATCH_RESULT.SECOND_UNMATCH
+        else:
+            return MATCH_RESULT.FIRST_UNMATCH
+
+
+def simulate_result_with_down_up(data_list) -> dict:
+    info("simulate_result_with_down_up..")
+    result = [down_up_strategy(40, 10, [a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
+    all_match_result = dict.fromkeys(MATCH_RESULT, 0)
+    for matchType in MATCH_RESULT:
+        all_match_result[matchType] = result.count(matchType)
+    return all_match_result
+
+
+def up_down_strategy(up_pt,down_pt, data):
+    # up_pt = 40
+    # down_pt = 10
     if data:
         start_pt = data[0]
         first_date = start_pt['timeObj']
         data_after = [x for x in data if x['timeObj'] > first_date]
         first_match = next((x for x in data_after if start_pt['low'] + up_pt * 0.0001 <= x['high']), None)
         debug(
-            f"Data - {start_pt['timeObj']}, dectect if data['high'] - {up_pt} * 0.0001 - {start_pt['high'] + up_pt * 0.0001}")
+            f"Data - {start_pt['timeObj']}, detect when get higher then {up_pt} * 0.0001 - {start_pt['high'] + up_pt * 0.0001}")
         if first_match is not None:
             debug(
                 f"First Match - {first_match['timeObj']}, try seek lower than {first_match['high'] - down_pt * 0.0001}")
@@ -79,9 +106,7 @@ def process_data(data):
 
 def simulate_result_with_up_down(data_list) -> dict:
     info("simulate_result_with_up_down..")
-    result = [process_data([a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
-    # worker_tasks = [pool.apply_async(f, [1]) for x in data_list]
-    #result = [worker_task.get() for worker_task in worker_tasks]
+    result = [up_down_strategy(40, 10, [a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
     all_match_result = dict.fromkeys(MATCH_RESULT, 0)
     for matchType in MATCH_RESULT:
         all_match_result[matchType] = result.count(matchType)
@@ -97,7 +122,9 @@ def simulate_day_trade(all_data) -> dict:
     grouped_data = {k: [data for data in g] for k, g in
                    groupby(sorted(all_data, key=itemgetter('timeGroup')), key=itemgetter('timeGroup'))}
     final_all_match_result = dict.fromkeys(MATCH_RESULT, 0)
-    [merge_match_result(final_all_match_result, simulate_result) for simulate_result in [simulate_result_with_up_down(date_list) for date_group, date_list in grouped_data.items()]]
+    # [merge_match_result(final_all_match_result, simulate_result) for simulate_result in [simulate_result_with_up_down(date_list) for date_group, date_list in grouped_data.items()]]
+    [merge_match_result(final_all_match_result, simulate_result) for simulate_result in
+     [simulate_result_with_down_up(date_list) for date_group, date_list in grouped_data.items()]]
     return final_all_match_result;
 
 
@@ -126,8 +153,8 @@ if __name__ == '__main__':
     rawJson = market_data_of('CADUSD', 1, '2019-08-21', '2020-08-07')
     [enrich(x) for x in rawJson]
     all_date_trade_match_result = simulate_day_trade(rawJson)
-    all_week_trade_match_result = simulate_week_trade(rawJson)
-    all_month_trade_match_result = simulate_month_trade(rawJson)
+    # all_week_trade_match_result = simulate_week_trade(rawJson)
+    # all_month_trade_match_result = simulate_month_trade(rawJson)
     show_result("date trade", all_date_trade_match_result)
-    show_result("week trade", all_week_trade_match_result)
-    show_result("month trade", all_month_trade_match_result)
+    # show_result("week trade", all_week_trade_match_result)
+    # show_result("month trade", all_month_trade_match_result)
