64c6910846ae67f162df651e4f436a806eb757a9
ahfish <ahfish@gmail.com>
2020-09-13 18:00:17 +0800

    file

--- main.py
+++ main.py
@@ -54,19 +54,19 @@ def enrich(data):
     data['monthGroup'] = date_time.strftime("%Y-%m")
 
 
-def down_up_strategy(down_pt, up_pt, data):
+def down_up_strategy(down_pt, up_pt, pt, data):
     if data:
         start_pt = data[0]
         first_date = start_pt['timeObj']
         data_after = [x for x in data if x['timeObj'] > first_date]
-        first_match = next((x for x in data_after if start_pt['high'] - down_pt * 0.0001 >= x['low']), None)
+        first_match = next((x for x in data_after if start_pt['high'] - down_pt * pt >= x['low']), None)
         debug(
-            f"Data - {start_pt['timeObj']}, detect when get lower than {start_pt['high'] - down_pt * 0.0001}")
+            f"Data - {start_pt['timeObj']}, detect when get lower than {start_pt['high'] - down_pt * pt}")
         if first_match is not None:
             debug(
-                f"First Match - {first_match['timeObj']}, try seek higher than {first_match['low'] + up_pt * 0.0001}")
+                f"First Match - {first_match['timeObj']}, try seek higher than {first_match['low'] + up_pt * pt}")
             list_after = [x for x in data_after if x['timeObj'] > first_match['timeObj']]
-            second_match = next((x for x in list_after if first_match['low'] + up_pt * 0.0001 <= x['high']), None)
+            second_match = next((x for x in list_after if first_match['low'] + up_pt * pt <= x['high']), None)
             if second_match is not None:
                 debug(f"Second Match - {second_match}")
                 return MATCH_RESULT.MATCH
@@ -76,28 +76,65 @@ def down_up_strategy(down_pt, up_pt, data):
             return MATCH_RESULT.FIRST_UNMATCH
 
 
-def simulate_result_with_down_up(data_list, down, up, code, duration) -> dict:
-    info("simulate_result_with_down_up..")
-    result = [down_up_strategy(down, up, [a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
-    all_match_result = dict.fromkeys(MATCH_RESULT, 0)
-    for matchType in MATCH_RESULT:
-        all_match_result[matchType] = result.count(matchType)
+def simulate_result_with_down_up(data_list, down, up, type, code, duration, pt) -> dict:
+    with postgresql() as conn:
+        info(f"{inspect.currentframe().f_code.co_name}...")
+        first_date = data_list[0]['timeObj']
+        last_date = data_list[-1]['timeObj']
+        all_match_result = dict.fromkeys(MATCH_RESULT, 0)
+        cached_result = conn.result_of(inspect.currentframe().f_code.co_name,
+            type,
+            code,
+            first_date,
+            last_date,
+            duration,
+            down,
+            up,
+            -1)
+        if len(cached_result) > 0:
+            all_match_result[MATCH_RESULT.FIRST_UNMATCH] = cached_result[0][0]
+            all_match_result[MATCH_RESULT.MATCH] = cached_result[0][1]
+            all_match_result[MATCH_RESULT.SECOND_UNMATCH] = cached_result[0][2]
+        else:
+            result = [down_up_strategy(down, up, pt, [a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
+            for matchType in MATCH_RESULT:
+                all_match_result[matchType] = result.count(matchType)
+            conn.insert_result(
+                inspect.currentframe().f_code.co_name,
+                type,
+                code,
+                first_date,
+                last_date,
+                duration,
+                down,
+                up,
+                -1,
+                all_match_result[MATCH_RESULT.FIRST_UNMATCH],
+                all_match_result[MATCH_RESULT.MATCH],
+                all_match_result[MATCH_RESULT.SECOND_UNMATCH]
+            )
     return all_match_result
+    #info("simulate_result_with_down_up..")
+    #result = [down_up_strategy(down, up, pt, [a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
+    #all_match_result = dict.fromkeys(MATCH_RESULT, 0)
+    #for matchType in MATCH_RESULT:
+    #    all_match_result[matchType] = result.count(matchType)
+    #return all_match_result
 
 
-def up_down_strategy(up_pt, down_pt, data):
+def up_down_strategy(up_pt, down_pt, pt, data):
     if data:
         start_pt = data[0]
         first_date = start_pt['timeObj']
         data_after = [x for x in data if x['timeObj'] > first_date]
-        first_match = next((x for x in data_after if start_pt['low'] + up_pt * 0.0001 <= x['high']), None)
+        first_match = next((x for x in data_after if start_pt['low'] + up_pt * pt <= x['high']), None)
         debug(
-            f"Data - {start_pt['timeObj']}, detect when get higher then {up_pt} * 0.0001 - {start_pt['high'] + up_pt * 0.0001}")
+            f"Data - {start_pt['timeObj']}, detect when get higher then {up_pt} * pt - {start_pt['high'] + up_pt * pt}")
         if first_match is not None:
             debug(
-                f"First Match - {first_match['timeObj']}, try seek lower than {first_match['high'] - down_pt * 0.0001}")
+                f"First Match - {first_match['timeObj']}, try seek lower than {first_match['high'] - down_pt * pt}")
             list_after = [x for x in data_after if x['timeObj'] > first_match['timeObj']]
-            second_match = next((x for x in list_after if first_match['high'] - down_pt * 0.0001 >= x['low']), None)
+            second_match = next((x for x in list_after if first_match['high'] - down_pt * pt >= x['low']), None)
             if second_match is not None:
                 debug(f"Second Match - {second_match}")
                 return MATCH_RESULT.MATCH
@@ -107,7 +144,7 @@ def up_down_strategy(up_pt, down_pt, data):
             return MATCH_RESULT.FIRST_UNMATCH
 
 
-def simulate_result_with_up_down(data_list, up, down, type, code, duration) -> dict:
+def simulate_result_with_up_down(data_list, up, down, type, code, duration, pt) -> dict:
     with postgresql() as conn:
         info(f"{inspect.currentframe().f_code.co_name}...")
         first_date = data_list[0]['timeObj']
@@ -127,7 +164,7 @@ def simulate_result_with_up_down(data_list, up, down, type, code, duration) -> d
             all_match_result[MATCH_RESULT.MATCH] = cached_result[0][1]
             all_match_result[MATCH_RESULT.SECOND_UNMATCH] = cached_result[0][2]
         else:
-            result = [up_down_strategy(up, down, [a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
+            result = [up_down_strategy(up, down, pt, [a for a in data_list if a['timeObj'] > x['timeObj']]) for x in data_list]
             for matchType in MATCH_RESULT:
                 all_match_result[matchType] = result.count(matchType)
             conn.insert_result(
@@ -152,12 +189,12 @@ def merge_match_result(final_match_result, match_result):
         final_match_result[matchType] += match_result[matchType]
 
 
-def simulate_day_trade(all_data, func, first_argument, second_argument, code) -> dict:
+def simulate_day_trade(all_data, func, first_argument, second_argument, code, pt) -> dict:
     grouped_data = {k: [data for data in g] for k, g in
                     groupby(sorted(all_data, key=itemgetter('timeGroup')), key=itemgetter('timeGroup'))}
     final_all_match_result = dict.fromkeys(MATCH_RESULT, 0)
     [merge_match_result(final_all_match_result, simulate_result) for simulate_result in
-     [func(date_list, first_argument, second_argument, date_group.strftime("%Y-%m-%d"), code, 'day') for date_group, date_list in grouped_data.items()]]
+     [func(date_list, first_argument, second_argument, date_group.strftime("%Y-%m-%d"), code, 'day', pt) for date_group, date_list in grouped_data.items()]]
     return final_all_match_result
 
 
@@ -185,12 +222,19 @@ def show_result(type, result):
 
 
 if __name__ == '__main__':
-    code = 'CADUSD'
-    rawJson = market_data_of(code, 1, '2019-08-21', '2020-08-07')
+    code = 'AUDUSD'
+    rawJson = market_data_of(code, 1, '2017-12-04', '2020-09-06')
     [enrich(x) for x in rawJson]
-    all_date_trade_match_result = simulate_day_trade(rawJson, simulate_result_with_up_down, 40, 10, code)
+    for first in range(20):
+        for second in range(first):
+            all_date_trade_match_result = simulate_day_trade(rawJson, simulate_result_with_down_up, (first+1)*10, (second+1)*10, code,
+                                                             0.0001)
+            all_date_trade_match_result = simulate_day_trade(rawJson, simulate_result_with_up_down, (first+1)*10, (second+1)*10, code,
+                                                             0.0001)
+    #all_date_trade_match_result = simulate_day_trade(rawJson, simulate_result_with_up_down, 40, 10, code, 0.0001)
+
     # all_week_trade_match_result = simulate_week_trade(rawJson)
     # all_month_trade_match_result = simulate_month_trade(rawJson)
-    show_result("date trade", all_date_trade_match_result)
+    #show_result("date trade", all_date_trade_match_result)
     # show_result("week trade", all_week_trade_match_result)
     # show_result("month trade", all_month_trade_match_result)
